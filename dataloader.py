055be5a47e9f2b00f5611c3205146ce0485129ac
Arnab Pushilal <arnab.pushilal.21@ucl.ac.uk>
2021-12-19 18:01:06 +0000

    added transform

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,4 @@
+
 import h5py
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
@@ -14,7 +15,20 @@ class H5ImageLoader(Dataset):
 
     """
 
-    def __init__(self,img_file,mask_file,bbox_file,classification_file):
+    def __init__(self,img_file,mask_file,bbox_file,classification_file,transform=None):
+        
+        """
+
+        @params:
+        img_file(string): Path for images
+        mask_file(string): Path for corresponding masks
+        bbox_file(string): Path for bounding boxes 
+        classification_file(string): Path for classes (0 or 1)
+        
+        transform(callable): Transform to be applied to the images ## ONLY TRAINING IMAGES, MASKS? ( I DONT THINK SO)
+        """
+
+  
         self.img_h5 = h5py.File(img_file,'r')
         self.mask_h5=h5py.File(mask_file,'r')
         self.bbox_h5=h5py.File(bbox_file,'r')
@@ -24,6 +38,8 @@ class H5ImageLoader(Dataset):
         self.mask_list = list(self.mask_h5.keys())[0]
         self.bbox_list = list(self.bbox_h5.keys())[0]
         self.classification_list = list(self.classifcation_h5.keys())[0]
+
+        self.transform=transform
 
 
     def __len__(self):
@@ -35,28 +51,29 @@ class H5ImageLoader(Dataset):
       bbox=self.bbox_h5[self.bbox_list][idx]
       classification=self.classifcation_h5[self.classification_list][idx]
 
+      if self.transform:
+        image=self.transform(image)
+
       return image,{'mask':mask, 'bbox':bbox, 'classification':classification}
 
-DATA_PATH='data/train'
-t=H5ImageLoader(DATA_PATH+'/images.h5',DATA_PATH+'/masks.h5',DATA_PATH+'/bboxes.h5',DATA_PATH+'/binary.h5') #All data paths 
-dataloader = DataLoader(t, batch_size=8, shuffle=True)
+if __name__=='__main__':
+ DATA_PATH='data/train'
+ t=H5ImageLoader(DATA_PATH+'/images.h5',DATA_PATH+'/masks.h5',DATA_PATH+'/bboxes.h5',DATA_PATH+'/binary.h5') #All data paths 
+ dataloader = DataLoader(t, batch_size=8, shuffle=True)
 
 ### Showing examples of labels
 
-fig, ax = plt.subplots()
-train_features, trains = next(iter(dataloader))
-#print("Feature batch shape: ", train_features.size())
-print("Labels batch shape: ",trains['mask'].size())
-img = train_features[0]
-label = torch.squeeze(trains['mask'][0])
-#plt.imshow(img.to(torch.int32))
-X,Y,W,H =trains['bbox'][0]
-box = patches.Rectangle((X, Y), W, H, linewidth=1, edgecolor='b',facecolor='none') #To create the bounding box
+ fig, ax = plt.subplots()
+ train_features, trains = next(iter(dataloader))
+ #print("Feature batch shape: ", train_features.size())
+ print("Labels batch shape: ",trains['mask'].size())
+ img = train_features[0]
+ label = torch.squeeze(trains['mask'][0])
+ #plt.imshow(img.to(torch.int32))
+ X,Y,W,H =trains['bbox'][0]
+ box = patches.Rectangle((X, Y), W, H, linewidth=1, edgecolor='b',facecolor='none') #To create the bounding box
 
-ax.imshow(img.to(torch.int32))
-ax.add_patch(box)
-print(f"The class is trains{trains['classification'][0]}")
-plt.show()
-
-
-
+ ax.imshow(img.to(torch.int32))
+ ax.add_patch(box)
+ print(f"The class is trains{trains['classification'][0]}")
+ plt.show()
