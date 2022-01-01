e3e51efd76eadcbd509ad4373f7758a783d6e7a4
Sulakshana Chakraborty <Sulakshana.ch2014@gmail.com>
2022-01-01 20:25:30 +0000

    updated adding noise option in data loader

--- load_data.py
+++ load_data.py
@@ -23,17 +23,17 @@ def create_data_loaders(train_path, validation_path, test_path, batch_size=16, n
         [transforms.ToTensor(),
          transforms.Normalize((127.5, 127.5, 127.5), (127.5, 127.5, 127.5)),
          ])
-    test_loader = build_data_loader(data_path=test_path, pt_transforms=test_transform, batch_size=batch_size)
+    test_loader = build_data_loader(data_path=test_path, pt_transforms=test_transform, batch_size=batch_size, noisy=False)
 
     return train_loader, validation_loader, test_loader
 
 
 def build_data_loader(data_path, pt_transforms, batch_size=16, noisy=False):
     if noisy:
-        images_filepath = pathlib.Path(data_path + '/nois_data.h5')
-    else:
-        # Define paths
-        images_filepath = pathlib.Path(data_path + '/images.h5')
+        images_noisy_filepath = pathlib.Path(data_path + '/noisy_data.h5')
+    else: images_noisy_filepath = None
+
+    images_filepath = pathlib.Path(data_path + '/images.h5')        
 
     masks_filepath = pathlib.Path(data_path + '/masks.h5')
     bboxes_filepath = pathlib.Path(data_path + '/bboxes.h5')
@@ -42,7 +42,7 @@ def build_data_loader(data_path, pt_transforms, batch_size=16, noisy=False):
     # Create loader
     image_loader = H5ImageLoader(img_file=images_filepath, mask_file=masks_filepath, bbox_file=bboxes_filepath,
                                  classification_file=labels_filepath, transform=pt_transforms,
-                                 noisy=noisy)  # All data paths
+                                 noisy=noisy,noisy_file = images_noisy_filepath)  # All data paths
     # Create pytorch loader
     data_loader = DataLoader(image_loader, batch_size=batch_size, shuffle=True)
 
@@ -65,7 +65,7 @@ class H5ImageLoader(Dataset):
 
     """
 
-    def __init__(self, img_file, mask_file, bbox_file, classification_file, transform, noisy):
+    def __init__(self, img_file, mask_file, bbox_file, classification_file, transform, noisy,noisy_file = None):
         """
 
         @params:
@@ -78,11 +78,18 @@ class H5ImageLoader(Dataset):
         """
 
         self.img_h5 = h5py.File(img_file, 'r')
+        self.dataset_list = list(self.img_h5.keys())[0]
+
+        self.noisy = noisy
+        if noisy: 
+            self.add_noise(noisy_file)
+            self.img_noisy_h5 = h5py.File(noisy_file, 'r')
+            self.dataset_noisy_list = list(self.img_noisy_h5.keys())[0]
+
+
         self.mask_h5 = h5py.File(mask_file, 'r')
         self.bbox_h5 = h5py.File(bbox_file, 'r')
         self.classifcation_h5 = h5py.File(classification_file, 'r')
-
-        self.dataset_list = list(self.img_h5.keys())[0]
         self.mask_list = list(self.mask_h5.keys())[0]
         self.bbox_list = list(self.bbox_h5.keys())[0]
         self.classification_list = list(self.classifcation_h5.keys())[0]
@@ -90,12 +97,16 @@ class H5ImageLoader(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return self.img_h5[list(self.img_h5.keys())[0]].shape[0]
+        return 5# self.img_h5[list(self.img_h5.keys())[0]].shape[0]
 
     # return 40
 
     def __getitem__(self, idx):
-        image = self.img_h5[self.dataset_list][idx]
+        if self.noisy:
+            image = self.img_noisy_h5[self.dataset_noisy_list][idx]
+        else:
+            image = self.img_h5[self.dataset_list][idx]
+
         mask = self.mask_h5[self.mask_list][idx]
         bbox = self.bbox_h5[self.bbox_list][idx]
         classification = self.classifcation_h5[self.classification_list][idx]
@@ -111,10 +122,11 @@ class H5ImageLoader(Dataset):
 
         return image, {'mask': mask, 'bbox': bbox, 'classification': classification}
 
-    def add_noise(self, std=1.0, mean=0.0):
+    def add_noise(self, filepath = None, std=1.0, mean=0.0):
         # convert h5 to numpy
         hf = self.img_h5
-        data = hf.get(self.dataset_list).value
+        h = self.dataset_list
+        data = hf[self.dataset_list][:]
 
         # # img =  # numpy-array of shape (N, M); dtype=np.uint8
         # # ...
@@ -123,7 +135,7 @@ class H5ImageLoader(Dataset):
         noisy_img = data + np.random.normal(mean, std, data.shape)
         noisy_img_clipped = np.clip(noisy_img, 0, 255)  # we might get out of bounds due to noise
 
-        h5f = h5py.File('noisy_data.h5', 'w')
+        h5f = h5py.File(filepath, 'w')
         h5f.create_dataset("noisy_data", data=noisy_img_clipped)
 
         h5f.close()
