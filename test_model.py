4add14f5085871778f484c36e44e8d182647c5ef
SulakshanaChakraborty <68264975+SulakshanaChakraborty@users.noreply.github.com>
2022-01-13 11:33:08 +0000

    Update test_model.py
    Added denoising to testing

--- test_model.py
+++ test_model.py
@@ -136,6 +136,88 @@ def evaluate_color_on_data(test_loader, model, device, loss_criterion, model_nam
     print("F1s", round(np.mean(test_f1_arr), 3), file=file)
     print("AB", round(np.mean(test_ab_loss), 3), file=file)
     file.close()
+    
+    def evaluate_denoising(test_loader, model, device, loss_criterion, model_name=""):
+    # todo: implement test scheme.
+    test_loss = []
+    test_accuracy = []
+    test_iou=[]
+    test_jaca=[]
+    test_f1_arr=[]
+    test_denoise_jac =[]
+
+    test_bbox_loss=[]
+    test_segmentation_loss=[]
+    test_label_loss=[]
+    test_denoise_loss = []
+
+
+    # Evaluate model
+    # Compute Testing metrics
+    for i, batch_data in enumerate(test_loader, 1):
+        # TODO: recognise denoiing, colour,
+        #TODO recognise the metrics for which model
+        with torch.no_grad():
+            inputs, labels = batch_data
+            inputs = inputs.to(device)
+            mask = torch.squeeze(labels['mask'].to(device))
+            mask = mask.to(torch.long)
+            binary = torch.squeeze(labels['classification'].to(device))
+            binary = binary.to(torch.long)
+            bbox = labels['bbox'].to(device)
+
+            try:
+                denoised_target = labels['denoised'].to(device)
+            except: denoised_target = None
+
+            bbox=bbox.float()
+            classes, boxes, segmask,denoised_pred = model(inputs)
+            loss,labels_loss,segmentation_loss,bboxes_loss,denoise_loss= loss_criterion(input_labels=classes, input_segmentations=segmask, \
+                input_bboxes=boxes,input_denoise = denoised_pred, target_labels=binary, target_segmentations=mask,
+                target_bboxes=bbox,target_denoise =denoised_target)
+            
+            pred_ax=np.argmax(classes.detach().cpu().numpy(),axis=1)
+            test_accuracy.append(np.sum((binary.detach().cpu().numpy()==pred_ax).astype(int))/len(binary))    
+            test_loss.append(loss.item())
+
+            print(test_accuracy[i-1],"minibatch acc")
+
+            test_label_loss.append(labels_loss.data.item())
+            test_segmentation_loss.append(segmentation_loss.data.item())
+
+            test_bbox_loss.append(bboxes_loss.data.item())
+            target_segmentation = torch.argmax(segmask, 1)
+            iou=(eval_metrics(mask.cpu(),target_segmentation.cpu(),2))
+            test_iou.append(iou.item())
+            test_denoise_loss.append(denoise_loss.data.item())
+            
+            mask_array=np.array(mask.cpu()).ravel()
+            predicted_array=np.array(target_segmentation.cpu()).ravel()
+
+            #print(jaccard_score(mask_array,predicted_array,average='weighted'),'skjac segmentation')
+            # print(jaccard_score(denoise_pred_array,denoised_target_array,average='weighted'),'denoising segmentation')
+            #print(f1_score(mask_array,predicted_array),"skf1")
+
+            test_jac=jaccard_score(mask_array,predicted_array,average='weighted')
+            #test_jac_denoise = jaccard_score(denoise_pred_array,denoised_target_array,average='weighted')
+            test_f1=f1_score(mask_array,predicted_array)
+
+            test_jaca.append(test_jac)
+            test_denoise_jac.append(test_jaca)
+            test_f1_arr.append(test_f1)          
+
+    print("-----------------------Testing Metrics-------------------------------------------")
+    file = open("output.txt", "a")
+    print("Model Name: "+str(model_name),file=file)
+    print("Loss: ", round(np.mean(test_loss), 3), "Test Accu: ", round(np.mean(test_accuracy), 3), file=file)
+    print("IOU: ", round(np.mean(test_iou), 3), file=file)
+    print("BBOX-loss: ", round(np.mean(test_bbox_loss), 3), file=file)
+    print("Segmnetaiton-loss", round(np.mean(test_segmentation_loss), 3), file=file)
+    print("Label-loss", round(np.mean(test_label_loss), 3), file=file)
+    print("Jac", round(np.mean(test_jaca), 3), file=file)
+    print("F1s", round(np.mean(test_f1_arr), 3), file=file)
+    print("Denoising-loss", round(np.mean(test_denoise_loss), 3), file=file)
+    file.close()
     # loss = 1
     # metrics = 2
     # return loss, metrics
