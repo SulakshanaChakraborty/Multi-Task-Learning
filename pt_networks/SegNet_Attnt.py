12ce046fd6c04a31d845128e370a3f24696d4285
michalpaw18 <72792980+michalpaw18@users.noreply.github.com>
2022-01-14 17:19:39 +0000

    Update SegNet_Attnt.py

--- pt_networks/SegNet_Attnt.py
+++ pt_networks/SegNet_Attnt.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SegNet(nn.Module):
+    """Segnet model with attention"""
     def __init__(self):
         super().__init__()
         filter = [64, 128, 256, 512, 512]
