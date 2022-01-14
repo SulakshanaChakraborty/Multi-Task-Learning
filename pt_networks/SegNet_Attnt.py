3ab6d5aa7fd7d25f319c4270f2907b82a5c01560
michalpaw18 <72792980+michalpaw18@users.noreply.github.com>
2022-01-14 19:36:31 +0000

    Update SegNet_Attnt.py

--- pt_networks/SegNet_Attnt.py
+++ pt_networks/SegNet_Attnt.py
@@ -48,7 +48,16 @@ class SegNet(nn.Module):
         self.flat=nn.Flatten()
     
     def conv2d_layer(self,in_ch,out_ch,kernel_size=3,padding=1,stride=1):
-
+        """A function creating a layer consisting of convolution, batch normalisation and relu.
+        Args:
+            in_ch (int): Number of input channels.
+            out_ch (int): Number of output channels.
+            padding (int, optional): Padding for the convolution stage. Default is 1.
+        
+        Returns:
+            A layer made up of three smaller layers.
+        
+        """
         layer=[]
         layer.append(nn.BatchNorm2d(in_ch))
         layer.append(nn.Conv2d(in_channels=in_ch,out_channels=out_ch,kernel_size=kernel_size,padding=padding,stride=stride))
@@ -57,6 +66,10 @@ class SegNet(nn.Module):
         return nn.Sequential(*layer)
 
     def att_layer(self, channel):
+        """A function used to create an attention block from individual layers including convolutions, batch normalisation, relu and sigmoid.
+        Returns:
+            attnt_block (pytorch object): A structure made up of smaller individual layers.
+        """
         att_block = nn.Sequential(
             nn.Conv2d(in_channels=channel[0], out_channels=channel[1], kernel_size=1, padding=0),
             nn.BatchNorm2d(channel[1]),
@@ -150,7 +163,16 @@ class Decoder(nn.Module):
 
 
     def conv2d_layer(self,in_ch,out_ch,kernel_size=3,padding=1,stride=1):
-
+        """A function creating an attention block consisting of convolution, batch normalisation and relu.
+        Args:
+            in_ch (int): Number of input channels.
+            out_ch (int): Number of output channels.
+            padding (int, optional): Padding for the convolution stage. Default is 1.
+        
+        Returns:
+            attnt_block (pytorch object): An attention block made up of three smaller layers.
+        
+        """
         layer=[]
         layer.append(nn.BatchNorm2d(in_ch))
         layer.append(nn.Conv2d(in_channels=in_ch,out_channels=out_ch,kernel_size=kernel_size,padding=padding,stride=stride))
