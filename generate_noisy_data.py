b3b6a4e8c4f5891dfd01629debbdac8319e77a75
michalpaw18 <72792980+michalpaw18@users.noreply.github.com>
2022-01-15 00:38:29 +0000

    Update generate_noisy_data.py
    unindented the code from line 18

--- generate_noisy_data.py
+++ generate_noisy_data.py
@@ -15,29 +15,29 @@ def add_noise(data_path, std_high=20, mean_high=10):
     Returns:
         images_noisy_filepath: file path of the noisy images.
     """
-        # convert h5 to numpy
+    # convert h5 to numpy
 
+    images_noisy_filepath = pathlib.Path(data_path + '/noisy_data.h5')
+    images_filepath = pathlib.Path(data_path + '/images.h5')
+
+    if not os.path.isfile(images_noisy_filepath):
+        images = h5py.File(images_filepath, 'r')
+
+        key = list(images.keys())[0]
+        img = images[key]     
+
+        #std_arr = np.random.uniform(low = 0,high =std_high,size = (img.shape[0],1,1,1))
+        # mean_arr = np.random.uniform(low = 0,high =mean_high,size = (img.shape[0],1,1,1))
+
+        noise_normal = np.random.normal(size = img.shape)
+        noise_random_normal = noise_normal*2 + 5   #*std_arr + mean_arr
+        noisy_img = img + noise_random_normal
         images_noisy_filepath = pathlib.Path(data_path + '/noisy_data.h5')
-        images_filepath = pathlib.Path(data_path + '/images.h5')
-        
-        if not os.path.isfile(images_noisy_filepath):
-            images = h5py.File(images_filepath, 'r')
-    
-            key = list(images.keys())[0]
-            img = images[key]     
-    
-            #std_arr = np.random.uniform(low = 0,high =std_high,size = (img.shape[0],1,1,1))
-            # mean_arr = np.random.uniform(low = 0,high =mean_high,size = (img.shape[0],1,1,1))
-    
-            noise_normal = np.random.normal(size = img.shape)
-            noise_random_normal = noise_normal*2 + 5   #*std_arr + mean_arr
-            noisy_img = img + noise_random_normal
-            images_noisy_filepath = pathlib.Path(data_path + '/noisy_data.h5')
-            noisy_img_clipped = np.clip(noisy_img, 0, 255)
-            h5_noisy = h5py.File(images_noisy_filepath, 'w')
-            h5_noisy.create_dataset("noisy_data", data=noisy_img_clipped)
-            h5_noisy.close()
-        return images_noisy_filepath
+        noisy_img_clipped = np.clip(noisy_img, 0, 255)
+        h5_noisy = h5py.File(images_noisy_filepath, 'w')
+        h5_noisy.create_dataset("noisy_data", data=noisy_img_clipped)
+        h5_noisy.close()
+    return images_noisy_filepath
 
 
 
