5118d3ee43593c35a33b528e1dbe0f00f61b8e74
michalpaw18 <72792980+michalpaw18@users.noreply.github.com>
2022-01-15 00:00:33 +0000

    Update cw2_main.py

--- cw2_main.py
+++ cw2_main.py
@@ -191,14 +191,14 @@ def run_cw2(args, train=True, test=False, visualize=True):
 
 
 def process_args():
-"""A function used to customise the running of the script.
-  Various options are available such as the model type, device, batch size, mode of running (ie: training or testing) and 
-  visualisation.
+    """A function used to customise the running of the script.
+      Various options are available such as the model type, device, batch size, mode of running (ie: training or testing) and 
+      visualisation.
 
 
-  Returns:
-      Interprets the arguments added to the argument parser object during the running of the script.
-"""
+      Returns:
+          Interprets the arguments added to the argument parser object during the running of the script.
+    """
     ap = argparse.ArgumentParser(description="COMP0090 cw 2 script")
     ap.add_argument("-m", '--model_type',
                     help='type of model to build (baseline/mlt_hard/mlt_attention/denoising_attention/color_segnet)',
